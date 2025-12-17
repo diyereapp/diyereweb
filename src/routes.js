ba5b572525b523b8e39404ee5a8ead6b9a11803d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-12-17 09:23:03 -0800

    all

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 
 import { Navigate } from "react-router-dom";
-import AdminRoute from "./components/admindashboard/AdminRoute";
 import sessionRoutes from "./components/sessions/SessionRoutes";
 import SalesRoute from "./components/salesdashboard/SalesRoute";
 import ManagerRoute from "./components/managerdashboard/ManagerRoute";
@@ -20,7 +19,7 @@ const routes = [
   { path: "/oauth-callback", element: <GoogleAuthHandler /> },
 
   {
-    children: [...AdminRoute, ...SalesRoute, ...ManagerRoute],
+    children: [ ...SalesRoute, ...ManagerRoute],
   },
   ...sessionRoutes,
   {
